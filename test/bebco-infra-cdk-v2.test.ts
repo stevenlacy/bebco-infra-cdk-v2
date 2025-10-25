1d9434266eb3758fe415701a2e33dd0a1f6761ad
stevenlacy <steven@stevenlacy.info>
2025-10-25 14:01:26 -0700

    Initial commit

---
+++ test/bebco-infra-cdk-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BebcoInfraCdkV2 from '../lib/bebco-infra-cdk-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bebco-infra-cdk-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BebcoInfraCdkV2.BebcoInfraCdkV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
